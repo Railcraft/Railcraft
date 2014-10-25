a0a1e7e72f7b5affaac318ee5e2795a72289973c
CovertJaguar <covertjaguar@gmail.com>
2014-10-25 03:57:59 -0700

    Add tooltips to some of the carts

--- src/main/java/mods/railcraft/common/carts/ItemCart.java
+++ src/main/java/mods/railcraft/common/carts/ItemCart.java
@@ -11,6 +11,7 @@ package mods.railcraft.common.carts;
 import com.mojang.authlib.GameProfile;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import java.util.List;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.item.EntityMinecart;
 import net.minecraft.entity.player.EntityPlayer;
@@ -19,8 +20,8 @@ import net.minecraft.item.ItemMinecart;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import mods.railcraft.api.core.items.IMinecartItem;
-import mods.railcraft.common.core.Railcraft;
 import mods.railcraft.common.core.RailcraftConfig;
+import mods.railcraft.common.gui.tooltips.ToolTip;
 import mods.railcraft.common.util.misc.Game;
 import mods.railcraft.common.util.misc.MiscTools;
 import net.minecraft.block.BlockDispenser;
@@ -80,6 +81,14 @@ public class ItemCart extends ItemMinecart implements IMinecartItem {
     @Override
     public EntityMinecart placeCart(GameProfile owner, ItemStack cartStack, World world, int x, int y, int z) {
         return CartUtils.placeCart(type, owner, cartStack, world, x, y, z);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List info, boolean adv) {
+        super.addInformation(stack, player, info, adv);
+        ToolTip tip = ToolTip.buildToolTip(stack.getUnlocalizedName() + ".tip");
+        if (tip != null)
+            info.addAll(tip.convertToStrings());
     }
 
 }
