1ff46ff60dd1996e60ff50688ea6cae27a137034
wjykk <wooky@live.ca>
2015-01-12 21:27:59 -0700

    Regex parsing and such
    PS: never write to an array using condensed for loops - that data will be discarded and your program will quietly laugh at you for being so dumb.

--- src/main/java/mods/railcraft/client/gui/GuiBoxAnalogController.java
+++ src/main/java/mods/railcraft/client/gui/GuiBoxAnalogController.java
@@ -1,30 +1,31 @@
 package mods.railcraft.client.gui;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import mods.railcraft.api.signals.SignalAspect;
-import mods.railcraft.client.gui.buttons.GuiToggleButton;
 import mods.railcraft.common.blocks.signals.TileBoxAnalogController;
-import mods.railcraft.common.core.RailcraftConstants;
 import mods.railcraft.common.plugins.forge.LocalizationPlugin;
 import mods.railcraft.common.util.misc.Game;
 import mods.railcraft.common.util.network.PacketDispatcher;
 import mods.railcraft.common.util.network.PacketGuiReturn;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiTextField;
-import net.minecraft.util.ResourceLocation;
 
 public class GuiBoxAnalogController extends GuiBasic {
 
 	private final TileBoxAnalogController tile;
 	private final static int N_OF_ASPECTS = TileBoxAnalogController.N_OF_ASPECTS;
-	private String regex[];
+	private final static Pattern patternRange = Pattern.compile("(\\d+)-(\\d+)|(\\d+)");
+	
+	private boolean enableAspect[][];
 	private GuiTextField textbox[];
 	
 	public GuiBoxAnalogController(TileBoxAnalogController t)
 	{
 		super(t.getName());
 		tile = t;
-		regex = t.regex;
+		enableAspect = t.enableAspect;
 		textbox = new GuiTextField[N_OF_ASPECTS];
 	}
 	
@@ -42,7 +43,49 @@ public class GuiBoxAnalogController extends GuiBasic {
 		if(c < ' ' || (c >= '0' && c <= '9') || c == '-' || c == ',' || c > '~')
 			for(int j = 0; j < N_OF_ASPECTS; j++)
 				textbox[j].textboxKeyTyped(c, i);
-	};
+	}
+	
+	private String rangeToString(boolean b[]) {
+		String s = "";
+		int start = -1;
+		for(int i = 0; i < 16; i++) {
+			if(b[i]) {
+				if(start == -1) {
+					s += i;
+					start = i;
+				}
+			} else if(start != -1) {
+				if(i - 1 == start)
+					s += ",";
+				else
+					s += "-" + (i-1) + ",";
+				start = -1;
+			}
+		}
+		if(start != -1)
+			s += "-15";
+		return s;
+	}
+	
+	private boolean[] parseRegex(String regex) {
+		boolean b[] = new boolean[16];
+		Matcher m = patternRange.matcher(regex);
+		while(m.find()) {
+			if(m.groupCount() >= 3 && m.group(3) != null) {
+				int i = Integer.parseInt(m.group(3));
+				if(i >= 0 && i <= 15)
+					b[i] = true;
+			} else {
+				int start = Integer.parseInt(m.group(1));
+				int end = Integer.parseInt(m.group(2));
+				if(start >= 0 && end >= 0 && start <= 15 && end <= 15 && start <= end)
+					for(int i = start; i <= end; i++)
+						b[i] = true;
+			}
+		}
+		
+		return b;
+	}
 	
 	@Override
     public void initGui() {
@@ -57,6 +100,7 @@ public class GuiBoxAnalogController extends GuiBasic {
             textbox[i].setDisabledTextColour(-1);
             textbox[i].setEnableBackgroundDrawing(false);
         	textbox[i].setMaxStringLength(37);
+        	textbox[i].setText(rangeToString(enableAspect[i]));
         }
 
 	}
@@ -80,7 +124,9 @@ public class GuiBoxAnalogController extends GuiBasic {
 	@Override
     public void onGuiClosed() {
         if (Game.isNotHost(tile.getWorld())) {
-            //tile.enableAspect = enableAspect;
+        	for(int i = 0; i < N_OF_ASPECTS; i++)
+        		enableAspect[i] = parseRegex(textbox[i].getText());
+        	tile.enableAspect = enableAspect;
             PacketGuiReturn pkt = new PacketGuiReturn(tile);
             PacketDispatcher.sendToServer(pkt);
         }
--- src/main/java/mods/railcraft/common/blocks/signals/TileBoxAnalogController.java
+++ src/main/java/mods/railcraft/common/blocks/signals/TileBoxAnalogController.java
@@ -25,7 +25,6 @@ public class TileBoxAnalogController extends TileBoxBase implements IControllerT
     private int strongestSignal;
 
     public boolean enableAspect[][] = new boolean[N_OF_ASPECTS][16];
-    public String regex[] = new String[N_OF_ASPECTS];
 
     @Override
     public EnumSignal getSignalType() {
@@ -101,10 +100,6 @@ public class TileBoxAnalogController extends TileBoxBase implements IControllerT
         return aspect;
     }
     
-    private void parseRegex(String regex, int aspect) {
-    	
-    }
-
     @Override
     public void writeToNBT(NBTTagCompound data) {
         super.writeToNBT(data);
@@ -112,8 +107,10 @@ public class TileBoxAnalogController extends TileBoxBase implements IControllerT
 
         for (int i = 0; i < N_OF_ASPECTS; i++) {
             String n = SignalAspect.values()[i].toString();
-            for(int j = 0; j < 16; j++)
-            	data.setBoolean("enable_" + n + "_" + String.valueOf(j), enableAspect[i][j]);
+            int s = 0;
+            for(boolean b : enableAspect[i])
+            	s = (s << 1) | (b ? 1 : 0);
+            data.setShort("aspect_" + n, (short) s);
         }
 
         controller.writeToNBT(data);
@@ -126,8 +123,11 @@ public class TileBoxAnalogController extends TileBoxBase implements IControllerT
 
         for (int i = 0; i < N_OF_ASPECTS; i++) {
         	String n = SignalAspect.values()[i].toString();
-            for(int j = 0; j < 16; j++)
-            	enableAspect[i][j] = data.getBoolean("enable_" + n + "_" + String.valueOf(j));
+            short s = data.getShort("aspect_" + n);
+            for(int j = 0; j < 16; j++) {
+            	enableAspect[i][j] = (s & 0x8000) == 0x8000;
+            	s <<= 1;
+            }
         }
 
         controller.readFromNBT(data);
@@ -161,16 +161,16 @@ public class TileBoxAnalogController extends TileBoxBase implements IControllerT
 
     @Override
     public void writeGuiData(DataOutputStream data) throws IOException {
-        for(int i = 0; i < N_OF_ASPECTS; i++)
-    		for(int j = 0; j < 16; j++)
-    			data.writeBoolean(enableAspect[i][j]);
+        for(boolean c[] : enableAspect)
+        	for(boolean b : c)
+        		data.writeBoolean(b);
     }
 
     @Override
     public void readGuiData(DataInputStream data, EntityPlayer sender) throws IOException {
-        for (int i = 0; i < N_OF_ASPECTS; i++)
-            for(int j = 0; j < 16; j++)
-        		enableAspect[i][j] = data.readBoolean();
+        for(int i = 0; i < N_OF_ASPECTS; i++)
+    		for(int j = 0; j < 16; j++)
+    			enableAspect[i][j] = data.readBoolean();
     }
 
     @Override
