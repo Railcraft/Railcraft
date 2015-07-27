c352b66cb7a758b7c47c54cf50db4187cbd36806
Dunk Fordyce <dunkfordyce@gmail.com>
2015-07-27 19:13:33 +0100

    remove weird redstone checks - allows using conduits and wires from other mods

--- src/main/java/mods/railcraft/common/blocks/machine/gamma/TileLoaderBase.java
+++ src/main/java/mods/railcraft/common/blocks/machine/gamma/TileLoaderBase.java
@@ -123,11 +123,7 @@ public abstract class TileLoaderBase extends TileMachineItem implements IHasCart
 
     @Override
     public final boolean isPoweringTo(int side) {
-        if (!isPowered())
-            return false;
-        ForgeDirection opSide = MiscTools.getOppositeSide(side);
-        Block block = WorldPlugin.getBlockOnSide(worldObj, xCoord, yCoord, zCoord, opSide);
-        return TrackTools.isRailBlock(block) || block == Blocks.redstone_wire || block == Blocks.powered_repeater || block == Blocks.unpowered_repeater;
+        return isPowered();
     }
 
     @Override
