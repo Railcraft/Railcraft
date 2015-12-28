Railcraft/Railcraft

commit ff0fa1caa685deaf9f548a6535cc25bc2ec6f2f4

Added H.S. explosion exclusion list
Closes https://github.com/CovertJaguar/Railcraft/issues/756
Added exclusion config option
Added IMC for exclusion list
Cleaned up comment code for configs

src/main/java/mods/railcraft/common/carts/MinecartHooks.java
@@ -353,8 +353,12 @@ public final class MinecartHooks implements IMinecartCollisionHandler {
                 boolean otherHighSpeed = other.getEntityData().getBoolean("HighSpeed");
                 if (!otherHighSpeed || (cart.motionX > 0 ^ other.motionX > 0) || (cart.motionZ > 0 ^ other.motionZ > 0))
                     primeToExplode(cart);
-            } else
-                primeToExplode(cart);
+            }
+
+            if (RailcraftConfig.isEntityExcludedFromHighSpeedExplosions(other))
+                return;
+
+            primeToExplode(cart);
         }
     }
 

src/main/java/mods/railcraft/common/core/Railcraft.java
@@ -17,19 +17,13 @@ import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.*;
 import cpw.mods.fml.common.registry.GameRegistry;
-
-import java.io.File;
-
-import mods.railcraft.common.commands.RootCommand;
-import mods.railcraft.common.modules.ModuleCore;
-import net.minecraft.command.CommandHandler;
-import org.apache.logging.log4j.Level;
 import mods.railcraft.api.crafting.IRockCrusherRecipe;
 import mods.railcraft.api.crafting.RailcraftCraftingManager;
 import mods.railcraft.api.fuel.FuelManager;
 import mods.railcraft.common.blocks.aesthetics.lantern.BlockLantern;
 import mods.railcraft.common.blocks.anvil.BlockRCAnvil;
 import mods.railcraft.common.carts.LinkageManager;
+import mods.railcraft.common.commands.RootCommand;
 import mods.railcraft.common.fluids.RailcraftFluids;
 import mods.railcraft.common.items.ItemMagnifyingGlass;
 import mods.railcraft.common.items.firestone.BlockFirestoneRecharge;
@@ -42,11 +36,15 @@ import mods.railcraft.common.util.misc.Game;
 import mods.railcraft.common.util.misc.MiscTools;
 import mods.railcraft.common.util.network.PacketHandler;
 import net.minecraft.block.Block;
+import net.minecraft.command.CommandHandler;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidRegistry;
+import org.apache.logging.log4j.Level;
+
+import java.io.File;
 
 @Mod(modid = Railcraft.MOD_ID, name = "Railcraft",
         version = Railcraft.VERSION,
@@ -136,6 +134,15 @@ public final class Railcraft {
                         NBTTagCompound outputNBT = nbt.getCompoundTag("output" + i);
                         recipe.addOutput(ItemStack.loadItemStackFromNBT(outputNBT), outputNBT.getFloat("chance"));
                     }
+                }
+            } else if (mess.key.equals("high-speed-explosion-excluded-entities")) {
+                NBTTagCompound nbt = mess.getNBTValue();
+                if (nbt.hasKey("entities")) {
+                    String entities = nbt.getString("entities");
+                    Iterable<String> split = splitter.split(entities);
+                    RailcraftConfig.excludedAllEntityFromHighSpeedExplosions(split);
+                } else {
+                    Game.log(Level.WARN, "Mod %s attempted to exclude an entity from H.S. explosions, but failed: %s", mess.getSender(), nbt);
                 }
             }
         }

src/main/java/mods/railcraft/common/core/RailcraftConfig.java
@@ -35,6 +35,8 @@ import mods.railcraft.common.util.collections.ItemMap;
 import mods.railcraft.common.util.misc.Game;
 import mods.railcraft.common.util.misc.MiscTools;
 import mods.railcraft.common.util.steam.Steam;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
 import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
@@ -67,6 +69,7 @@ public class RailcraftConfig {
     private static final String CAT_TWEAKS_BLOCKS = CAT_TWEAKS + ".blocks";
     private static final String CAT_TWEAKS_ITEMS = CAT_TWEAKS + ".items";
     private static final String CAT_TWEAKS_ROUTING = CAT_TWEAKS + ".routing";
+    private static final Set<String> entitiesExcludedFromHighSpeedExplosions = new HashSet<String>();
     private static final Map<String, Boolean> enabledItems = new HashMap<String, Boolean>();
     private static final Map<String, Boolean> enabledBlocks = new HashMap<String, Boolean>();
     private static final Map<String, Boolean> carts = new HashMap<String, Boolean>();
@@ -250,7 +253,11 @@ public class RailcraftConfig {
         maxHighSpeed = get(CAT_TWEAKS_TRACKS + ".speed", "max.speed", 0.6f, 0.8f, 1.2f, "change '{t}' to limit max speed on high speed rails, useful if your computer can't keep up with chunk loading, min=0.6, default=0.8, max=1.2");
 
         launchRailMaxForce = get(CAT_TWEAKS_TRACKS + ".launch", "force.max", 5, 30, 50, "change the value to your desired max launch rail force, min=5, default=30, max=50");
+
+        String[] strings = get(CAT_TWEAKS_TRACKS + ".speed", "entities.excluded", new String[0], "add entity names to exclude them from explosions caused by high speed collisions");
+        Collections.addAll(entitiesExcludedFromHighSpeedExplosions, strings);
     }
+
 
     private static void loadRoutingTweaks() {
         routingOpsOnly = get(CAT_TWEAKS_ROUTING, "ops.only", false, "change to '{t}=true' to limit the editing of Golden Tickets to server admins only");
@@ -955,6 +962,21 @@ public class RailcraftConfig {
         return gen;
     }
 
+    public static boolean isEntityExcludedFromHighSpeedExplosions(Entity entity) {
+        String entityString = EntityList.getEntityString(entity);
+        return entitiesExcludedFromHighSpeedExplosions.contains(entityString);
+    }
+
+    public static void excludedAllEntityFromHighSpeedExplosions(Iterable<String> iterable) {
+        for (String entityName : iterable) {
+            excludedEntityFromHighSpeedExplosions(entityName);
+        }
+    }
+
+    public static void excludedEntityFromHighSpeedExplosions(String entityName) {
+        entitiesExcludedFromHighSpeedExplosions.add(entityName);
+    }
+
     private static List<Integer> getIntegerList(String cat, String tag, int maxEntries) {
         Property prop = configMain.get(cat, tag, "");
         String value = prop.getString();
@@ -977,9 +999,8 @@ public class RailcraftConfig {
     }
 
     private static void loadRecipeProperty(String subcat, String tag, boolean defaultValue, String comment) {
-        comment = COMMENT_PREFIX + comment.replace("{t}", tag) + COMMENT_SUFFIX;
         Property prop = configMain.get(CAT_RECIPES + "." + subcat, tag, defaultValue);
-        prop.comment = comment;
+        decorateComment(prop, tag, comment);
         recipes.put(subcat + "." + tag, prop.getBoolean(defaultValue));
     }
 
@@ -988,9 +1009,8 @@ public class RailcraftConfig {
     }
 
     private static boolean get(String cat, String tag, boolean defaultValue, boolean reset, String comment) {
-        comment = COMMENT_PREFIX + comment.replace("{t}", tag) + COMMENT_SUFFIX;
         Property prop = configMain.get(cat, tag, defaultValue);
-        prop.comment = comment;
+        decorateComment(prop, tag, comment);
         boolean ret = prop.getBoolean(defaultValue);
         if (reset)
             prop.set(defaultValue);
@@ -1012,16 +1032,14 @@ public class RailcraftConfig {
     }
 
     private static int get(String cat, String tag, int defaultValue, String comment) {
-        comment = COMMENT_PREFIX + comment.replace("{t}", tag) + COMMENT_SUFFIX;
         Property prop = configMain.get(cat, tag, defaultValue);
-        prop.comment = comment;
+        decorateComment(prop, tag, comment);
         return parseInteger(prop, defaultValue);
     }
 
     private static int get(String cat, String tag, int min, int defaultValue, int max, String comment) {
-        comment = COMMENT_PREFIX + comment.replace("{t}", tag) + COMMENT_SUFFIX;
         Property prop = configMain.get(cat, tag, defaultValue);
-        prop.comment = comment;
+        decorateComment(prop, tag, comment);
         int parsed = parseInteger(prop, defaultValue);
         int clamped = Math.max(parsed, min);
         clamped = Math.min(clamped, max);
@@ -1031,15 +1049,20 @@ public class RailcraftConfig {
     }
 
     private static float get(String cat, String tag, float min, float defaultValue, float max, String comment) {
-        comment = COMMENT_PREFIX + comment.replace("{t}", tag) + COMMENT_SUFFIX;
         Property prop = configMain.get(cat, tag, defaultValue);
-        prop.comment = comment;
+        decorateComment(prop, tag, comment);
         double parsed = parseDouble(prop, defaultValue);
         double clamped = Math.max(parsed, min);
         clamped = Math.min(clamped, max);
         if (clamped != parsed)
             prop.set(clamped);
         return (float) clamped;
+    }
+
+    private static String[] get(String category, String tag, String[] defaultValues, String comment) {
+        Property property = configMain.get(category, tag, defaultValues, comment);
+        decorateComment(property, tag, comment);
+        return property.getStringList();
     }
 
     private static int parseInteger(Property prop, int defaultValue) {
@@ -1073,9 +1096,8 @@ public class RailcraftConfig {
     }
 
     private static Property get(String cat, String tag, String defaultValue, String comment) {
-        comment = COMMENT_PREFIX + comment.replace("{t}", tag) + COMMENT_SUFFIX;
         Property prop = configMain.get(cat, tag, defaultValue);
-        prop.comment = comment;
+        decorateComment(prop, tag, comment);
         return prop;
     }
 
@@ -1083,6 +1105,11 @@ public class RailcraftConfig {
         Property prop = configMain.get(CAT_LOOT, tag, defaultValue);
         int chance = parseInteger(prop, defaultValue);
         lootChances.put(tag, chance);
+    }
+
+    private static void decorateComment(Property property, String tag, String comment) {
+        comment = COMMENT_PREFIX + comment.replace("{t}", tag) + COMMENT_SUFFIX;
+        property.comment = comment;
     }
 
     private static void loadCartProperty(String tag) {

commit a1e6d570653754f4b5c0cd17a36f122c382ab806

Bugfix for logic error
 Bug introduced in: ff0fa1caa685deaf9f548a6535cc25bc2ec6f2f4

src/main/java/mods/railcraft/common/carts/MinecartHooks.java
@@ -351,8 +351,10 @@ public final class MinecartHooks implements IMinecartCollisionHandler {
 
             if (other instanceof EntityMinecart) {
                 boolean otherHighSpeed = other.getEntityData().getBoolean("HighSpeed");
-                if (!otherHighSpeed || (cart.motionX > 0 ^ other.motionX > 0) || (cart.motionZ > 0 ^ other.motionZ > 0))
+                if (!otherHighSpeed || (cart.motionX > 0 ^ other.motionX > 0) || (cart.motionZ > 0 ^ other.motionZ > 0)) {
                     primeToExplode(cart);
+                    return;
+                }
             }
 
             if (RailcraftConfig.isEntityExcludedFromHighSpeedExplosions(other))