547c8d81a18dc66d58f6b6cd086537c62cb41037
CovertJaguar <covertjaguar@gmail.com>
2016-02-27 09:03:46 -0800

    Minor tweak to Disposal Track PR #745

--- src/main/java/mods/railcraft/common/blocks/tracks/TrackDisposal.java
+++ src/main/java/mods/railcraft/common/blocks/tracks/TrackDisposal.java
@@ -8,18 +8,17 @@
  */
 package mods.railcraft.common.blocks.tracks;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
 import mods.railcraft.api.tracks.ITrackPowered;
 import mods.railcraft.common.carts.CartUtils;
 import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityMinecart;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class TrackDisposal extends TrackSuspended implements ITrackPowered {
 
@@ -76,6 +75,7 @@ public class TrackDisposal extends TrackSuspended implements ITrackPowered {
     public void setPowered(boolean powered) {
         this.powered = powered;
     }
+
     @Override
     public void writeToNBT(NBTTagCompound nbttagcompound) {
         super.writeToNBT(nbttagcompound);
@@ -97,8 +97,11 @@ public class TrackDisposal extends TrackSuspended implements ITrackPowered {
     @Override
     public void readPacketData(DataInputStream data) throws IOException {
         super.readPacketData(data);
-        powered = data.readBoolean();
-        markBlockNeedsUpdate();
+        boolean p = data.readBoolean();
+        if (p != powered) {
+            powered = p;
+            markBlockNeedsUpdate();
+        }
     }
 
 }
