989aa4faca571bbc9f265f332621dd93d9716afe
CovertJaguar <covertjaguar@gmail.com>
2016-03-19 00:47:41 -0700

    Update some file headers

--- src/main/java/mods/railcraft/common/util/inventory/InventorySorter.java
+++ src/main/java/mods/railcraft/common/util/inventory/InventorySorter.java
@@ -1,11 +1,12 @@
-/* 
- * Copyright (c) CovertJaguar, 2014 http://railcraft.info
- * 
+/*******************************************************************************
+ * Copyright (c) CovertJaguar, 2011-2016
+ * http://railcraft.info
+ *
  * This code is the property of CovertJaguar
  * and may only be used with explicit written
  * permission unless otherwise specified on the
  * license page at http://railcraft.info/wiki/info:license.
- */
+ ******************************************************************************/
 package mods.railcraft.common.util.inventory;
 
 import net.minecraft.inventory.IInventory;
