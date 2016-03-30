40df718641d43b8dbed669f66cb0b6625692ddf9
CovertJaguar <covertjaguar@gmail.com>
2016-03-29 18:45:06 -0700

    Fix TrackSuspended

--- src/main/java/mods/railcraft/common/blocks/tracks/instances/TrackSuspended.java
+++ src/main/java/mods/railcraft/common/blocks/tracks/instances/TrackSuspended.java
@@ -1,21 +1,23 @@
-/*******************************************************************************
- * Copyright (c) CovertJaguar, 2011-2016
- * http://railcraft.info
+/*
+ * Copyright (c) CovertJaguar, 2014 http://railcraft.info
  *
  * This code is the property of CovertJaguar
  * and may only be used with explicit written
  * permission unless otherwise specified on the
  * license page at http://railcraft.info/wiki/info:license.
- ******************************************************************************/
-package mods.railcraft.common.blocks.tracks.instances;
+ */
+package mods.railcraft.common.blocks.tracks;
 
 import mods.railcraft.api.tracks.ITrackCustomPlaced;
 import mods.railcraft.common.blocks.RailcraftBlocks;
-import mods.railcraft.common.blocks.tracks.EnumTrack;
-import mods.railcraft.common.blocks.tracks.TrackTools;
+import mods.railcraft.common.blocks.tracks.instances.TrackUnsupported;
+import mods.railcraft.common.plugins.forge.WorldPlugin;
 import mods.railcraft.common.util.misc.Game;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRailBase.EnumRailDirection;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
@@ -28,8 +30,8 @@ public class TrackSuspended extends TrackUnsupported implements ITrackCustomPlac
     }
 
     @Override
-    public void onBlockPlaced() {
-        super.onBlockPlaced();
+    public void onBlockPlacedBy(IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        super.onBlockPlacedBy(state, placer, stack);
         if (!isSupported())
             breakRail();
     }
@@ -37,16 +39,13 @@ public class TrackSuspended extends TrackUnsupported implements ITrackCustomPlac
     @Override
     public void onNeighborBlockChange(IBlockState state, Block neighborBlock) {
         World world = getWorld();
-        int i = tileEntity.xCoord;
-        int j = tileEntity.yCoord;
-        int k = tileEntity.zCoord;
+        BlockPos pos = tileEntity.getPos();
         if (isSupported()) {
             Block myBlock = RailcraftBlocks.getBlockTrack();
-            if (block != myBlock) {
-                world.notifyBlocksOfNeighborChange(i + 1, j, k, myBlock);
-                world.notifyBlocksOfNeighborChange(i - 1, j, k, myBlock);
-                world.notifyBlocksOfNeighborChange(i, j, k + 1, myBlock);
-                world.notifyBlocksOfNeighborChange(i, j, k - 1, myBlock);
+            if (neighborBlock != myBlock) {
+                for (EnumFacing side : EnumFacing.HORIZONTALS) {
+                    world.notifyBlockOfStateChange(pos.offset(side), myBlock);
+                }
             }
         } else
             breakRail();
@@ -54,46 +53,45 @@ public class TrackSuspended extends TrackUnsupported implements ITrackCustomPlac
 
     public void breakRail() {
         if (Game.isHost(getWorld()))
-            getWorld().func_147480_a(tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord, true);
+            getWorld().destroyBlock(getPos(), true);
     }
 
-    public boolean isSupportedRail(World world, int i, int j, int k, int meta) {
-        if (!TrackTools.isRailBlockAt(world, i, j, k))
+    public boolean isSupportedRail(World world, BlockPos pos, EnumRailDirection dir) {
+        if (!TrackTools.isRailBlockAt(world, pos))
             return false;
-        if (isSupportedBelow(world, i, j, k))
+        if (isSupportedBelow(world, pos))
             return true;
-        if (meta == EnumTrackMeta.NORTH_SOUTH.ordinal()) {
-            if (isSupportedBelow(world, i, j, k + 1))
+        if (dir == EnumRailDirection.NORTH_SOUTH) {
+            if (isSupportedBelow(world, pos.north()))
                 return true;
-            return isSupportedBelow(world, i, j, k - 1);
-        } else if (meta == EnumTrackMeta.EAST_WEST.ordinal()) {
-            if (isSupportedBelow(world, i + 1, j, k))
+            return isSupportedBelow(world, pos.south());
+        } else if (dir == EnumRailDirection.EAST_WEST) {
+            if (isSupportedBelow(world, pos.east()))
                 return true;
-            return isSupportedBelow(world, i - 1, j, k);
+            return isSupportedBelow(world, pos.west());
         }
         return false;
     }
 
-    public boolean isSupportedBelow(World world, int i, int j, int k) {
-        if (!world.blockExists(i, j, k))
+    public boolean isSupportedBelow(World world, BlockPos pos) {
+        if (!WorldPlugin.isBlockLoaded(world, pos))
             return true;
-        if (TrackTools.isRailBlockAt(world, i, j, k))
-            return world.isSideSolid(i, j - 1, k, EnumFacing.UP);
+        if (TrackTools.isRailBlockAt(world, pos))
+            return world.isSideSolid(pos.down(), EnumFacing.UP);
         return false;
     }
 
     public boolean isSupported() {
-        int meta = tileEntity.getBlockMetadata();
-        return isSupported(getWorld(), tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord, meta);
+        return isSupported(getWorld(), getPos(), TrackTools.getTrackDirection(getWorld(), getPos()));
     }
 
-    public boolean isSupported(World world, BlockPos pos, int meta) {
-        if (isSupportedRail(world, i, j, k, meta))
+    public boolean isSupported(World world, BlockPos pos, EnumRailDirection dir) {
+        if (isSupportedRail(world, pos, dir))
             return true;
-        if (meta == EnumTrackMeta.NORTH_SOUTH.ordinal())
-            return isSupportedRail(world, i, j, k + 1, meta) || isSupportedRail(world, i, j, k - 1, meta);
-        else if (meta == EnumTrackMeta.EAST_WEST.ordinal())
-            return isSupportedRail(world, i + 1, j, k, meta) || isSupportedRail(world, i - 1, j, k, meta);
+        if (dir == EnumRailDirection.NORTH_SOUTH)
+            return isSupportedRail(world, pos.north(), dir) || isSupportedRail(world, pos.south(), dir);
+        else if (dir == EnumRailDirection.EAST_WEST)
+            return isSupportedRail(world, pos.east(), dir) || isSupportedRail(world, pos.west(), dir);
         return false;
     }
 
@@ -105,7 +103,7 @@ public class TrackSuspended extends TrackUnsupported implements ITrackCustomPlac
 //        if(BlockRail.isRailBlockAt(world, i, j + 1, k)) {
 //            return false;
 //        }
-        if (isSupported(world, pos, 0) || isSupported(world, pos, 1))
+        if (isSupported(world, pos, EnumRailDirection.NORTH_SOUTH) || isSupported(world, pos, EnumRailDirection.EAST_WEST))
             return true;
         return world.isSideSolid(pos.down(), EnumFacing.UP);
     }
