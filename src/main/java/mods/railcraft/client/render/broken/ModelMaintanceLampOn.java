e742cc5659e04b9147300a246e423e1ebe7f8102
CovertJaguar <covertjaguar@gmail.com>
2016-06-06 17:50:46 -0700

    Don't need this.

--- src/main/java/mods/railcraft/client/render/broken/ModelMaintanceLampOn.java
+++ src/main/java/mods/railcraft/client/render/broken/ModelMaintanceLampOn.java
@@ -1,14 +1,15 @@
-/* 
- * Copyright (c) CovertJaguar, 2014 http://railcraft.info
- * 
- * This code is the property of CovertJaguar
- * and may only be used with explicit written
- * permission unless otherwise specified on the
- * license page at http://railcraft.info/wiki/info:license.
- */
-package mods.railcraft.client.render.models.carts;
+/*******************************************************************************
+ Copyright (c) CovertJaguar, 2011-2016
+ http://railcraft.info
 
-import mods.railcraft.client.render.broken.TexturedQuadAdv;
+ This code is the property of CovertJaguar
+ and may only be used with explicit written
+ permission unless otherwise specified on the
+ license page at http://railcraft.info/wiki/info:license.
+ ******************************************************************************/
+package mods.railcraft.client.render.broken;
+
+import mods.railcraft.client.render.models.carts.ModelMaintanceLampOff;
 import net.minecraft.client.model.ModelBox;
 import net.minecraft.client.model.TexturedQuad;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
