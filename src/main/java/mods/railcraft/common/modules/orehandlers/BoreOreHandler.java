088756b8c5e0ec5d9f9e621e73f73d584b0e934e
CovertJaguar <covertjaguar@gmail.com>
2016-10-06 16:52:00 -0700

    Fix #916 wrap Ore scanner in try block because of broken mods

--- src/main/java/mods/railcraft/common/modules/orehandlers/BoreOreHandler.java
+++ src/main/java/mods/railcraft/common/modules/orehandlers/BoreOreHandler.java
@@ -1,18 +1,18 @@
-/*******************************************************************************
- * Copyright (c) CovertJaguar, 2011-2016
- * http://railcraft.info
- *
- * This code is the property of CovertJaguar
- * and may only be used with explicit written
- * permission unless otherwise specified on the
- * license page at http://railcraft.info/wiki/info:license.
- ******************************************************************************/
+/*------------------------------------------------------------------------------
+ Copyright (c) CovertJaguar, 2011-2016
+ http://railcraft.info
+
+ This code is the property of CovertJaguar
+ and may only be used with explicit written
+ permission unless otherwise specified on the
+ license page at http://railcraft.info/wiki/info:license.
+ -----------------------------------------------------------------------------*/
 package mods.railcraft.common.modules.orehandlers;
 
 import mods.railcraft.common.carts.EntityTunnelBore;
 import mods.railcraft.common.util.inventory.InvTools;
 import mods.railcraft.common.util.misc.Game;
-import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -36,9 +36,13 @@ public class BoreOreHandler {
                         || oreClass.equals("treeLeaves")
         )) {
             Game.log(Level.DEBUG, "Automation Module: Ore Detected, adding to blocks Tunnel Bore can mine: {0}, id={1} meta={2}", oreClass, ore, ore.getItemDamage());
-            Block block = InvTools.getBlockFromStack(ore);
-            if (block != null)
-                EntityTunnelBore.addMineableBlock(block.getStateFromMeta(ore.getItemDamage()));
+            try {
+                IBlockState state = InvTools.getBlockStateFromStack(ore);
+                if (state != null)
+                    EntityTunnelBore.addMineableBlock(state);
+            } catch (Exception ex) {
+                Game.logThrowable(Level.DEBUG, 3, ex, "Automation Module: Failed to add Ore to Tunnel Bore mining list: {0}, id={1} meta={2}", oreClass, ore, ore.getItemDamage());
+            }
         }
     }
 
