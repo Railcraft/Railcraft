c6a49548d5b7da1755ab515ba9bf65b3b2ca4265
CovertJaguar <covertjaguar@gmail.com>
2016-10-07 16:29:53 -0700

    Fixes #921 don't derp the item metadata, talk directly to JEI instead

---
+++ src/main/java/mods/railcraft/common/plugins/jei/RailcraftJEIPlugin.java
@@ -0,0 +1,34 @@
+/*------------------------------------------------------------------------------
+ Copyright (c) CovertJaguar, 2011-2016
+ http://railcraft.info
+
+ This code is the property of CovertJaguar
+ and may only be used with explicit written
+ permission unless otherwise specified on the
+ license page at http://railcraft.info/wiki/info:license.
+ -----------------------------------------------------------------------------*/
+
+package mods.railcraft.common.plugins.jei;
+
+import mezz.jei.api.BlankModPlugin;
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.ISubtypeRegistry;
+import mezz.jei.api.JEIPlugin;
+import mods.railcraft.common.blocks.RailcraftBlocks;
+import mods.railcraft.common.blocks.tracks.outfitted.ItemTrackOutfitted;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Created by CovertJaguar on 10/7/2016 for Railcraft.
+ *
+ * @author CovertJaguar <http://www.railcraft.info>
+ */
+@JEIPlugin
+public class RailcraftJEIPlugin extends BlankModPlugin {
+    @Override
+    public void register(@Nonnull IModRegistry registry) {
+        ISubtypeRegistry subtypeRegistry = registry.getJeiHelpers().getSubtypeRegistry();
+        subtypeRegistry.registerNbtInterpreter(RailcraftBlocks.TRACK_OUTFITTED.item(), stack -> ((ItemTrackOutfitted) stack.getItem()).getSuffix(stack));
+    }
+}
