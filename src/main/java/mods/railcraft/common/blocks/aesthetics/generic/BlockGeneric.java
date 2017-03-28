4d3cea8ffe75ef9a1a64d98d3a8407f27bf0fad9
GeneralCamo <cbrittain10@yahoo.com>
2017-03-28 11:26:12 -0400

    Corrected omissions to Aesthetic Code

--- src/main/java/mods/railcraft/common/blocks/aesthetics/generic/BlockGeneric.java
+++ src/main/java/mods/railcraft/common/blocks/aesthetics/generic/BlockGeneric.java
@@ -66,6 +66,7 @@ public class BlockGeneric extends BlockRailcraftSubtyped<EnumGeneric> {
         HarvestPlugin.setStateHarvestLevel("pickaxe", 1, EnumGeneric.BLOCK_LEAD);
         HarvestPlugin.setStateHarvestLevel("pickaxe", 1, EnumGeneric.BLOCK_TIN);
         HarvestPlugin.setStateHarvestLevel("pickaxe", 1, EnumGeneric.BLOCK_COPPER);
+        HarvestPlugin.setStateHarvestLevel("pickaxe", 1, EnumGeneric.BLOCK_BRONZE);
 
         HarvestPlugin.setStateHarvestLevel("axe", 0, EnumGeneric.BLOCK_CREOSOTE);
         HarvestPlugin.setStateHarvestLevel("shovel", 3, EnumGeneric.CRUSHED_OBSIDIAN);
@@ -78,6 +79,7 @@ public class BlockGeneric extends BlockRailcraftSubtyped<EnumGeneric> {
         ForestryPlugin.addBackpackItem("forestry.miner", EnumGeneric.BLOCK_STEEL.getStack());
         ForestryPlugin.addBackpackItem("forestry.miner", EnumGeneric.BLOCK_TIN.getStack());
         ForestryPlugin.addBackpackItem("forestry.miner", EnumGeneric.BLOCK_SILVER.getStack());
+        ForestryPlugin.addBackpackItem("forestry.miner", EnumGeneric.BLOCK_BRONZE.getStack());
 
         ForestryPlugin.addBackpackItem("forestry.builder", EnumGeneric.BLOCK_CONCRETE.getStack());
         ForestryPlugin.addBackpackItem("forestry.builder", EnumGeneric.BLOCK_CREOSOTE.getStack());
@@ -206,6 +208,7 @@ public class BlockGeneric extends BlockRailcraftSubtyped<EnumGeneric> {
             case BLOCK_STEEL:
             case BLOCK_TIN:
             case BLOCK_SILVER:
+            case BLOCK_BRONZE:
                 return SoundType.METAL;
             case BLOCK_CREOSOTE:
                 return SoundType.WOOD;
