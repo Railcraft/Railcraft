8d1d55dcf6aa60febf59268954aafbc2033a0e56
CovertJaguar <covertjaguar@gmail.com>
2017-06-02 14:30:11 -0700

    Simplify Brass Casing recipe def

--- src/main/java/mods/railcraft/common/modules/ModuleForestry.java
+++ src/main/java/mods/railcraft/common/modules/ModuleForestry.java
@@ -78,16 +78,13 @@ public class ModuleForestry extends RailcraftModulePayload {
                             '|', Items.STICK);
                 }
 
-                if (Metal.BRASS.getStack(Metal.Form.INGOT) != InvTools.emptyStack() && (RailcraftConfig.getRecipeConfig("forestry.misc.brass.casing"))) {
+                if (RailcraftConfig.getRecipeConfig("forestry.misc.brass.casing")) {
                         ItemStack casing = ModItems.STURDY_CASING.get();
-
-                    if (!InvTools.isEmpty(casing)){
                         CraftingPlugin.addRecipe(casing,
                                 "III",
                                 "I I",
                                 "III",
-                                'I', Metal.BRASS.getStack(Metal.Form.INGOT));
-                    }
+                                'I', RailcraftItems.INGOT, Metal.BRASS);
                 }
             }
         });
