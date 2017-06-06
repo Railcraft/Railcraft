4173bca99596dc96d9a8bdf31c29e313a4be7eed
liach <liach@users.noreply.github.com>
2017-06-06 11:17:02 -0700

    Improved buffer stop track hitbox detection
    Tested both track kits, they are fine.
    Fixes #1221

--- src/main/java/mods/railcraft/common/blocks/tracks/outfitted/kits/TrackBufferStop.java
+++ src/main/java/mods/railcraft/common/blocks/tracks/outfitted/kits/TrackBufferStop.java
@@ -9,6 +9,7 @@
  -----------------------------------------------------------------------------*/
 package mods.railcraft.common.blocks.tracks.outfitted.kits;
 
+import com.google.common.collect.Lists;
 import mods.railcraft.api.tracks.ITrackKitCustomShape;
 import mods.railcraft.api.tracks.ITrackKitReversible;
 import mods.railcraft.common.blocks.tracks.outfitted.TrackKits;
@@ -17,12 +18,17 @@ import mods.railcraft.common.util.misc.MiscTools;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.IBlockAccess;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.Nullable;
 
 public class TrackBufferStop extends TrackKitRailcraft implements ITrackKitReversible, ITrackKitCustomShape {
 
@@ -48,11 +54,32 @@ public class TrackBufferStop extends TrackKitRailcraft implements ITrackKitRever
     }
 
     @Override
-    public RayTraceResult collisionRayTrace(Vec3d vec3d, Vec3d vec3d1) {
-        return MiscTools.rayTraceBlock(vec3d, vec3d1, getPos());
+    @Nullable
+    public RayTraceResult collisionRayTrace(Vec3d start, Vec3d end) {
+        IBlockAccess world = theWorldAsserted();
+        BlockPos pos = getPos();
+        List<RayTraceResult> list = Lists.newArrayList(
+            MiscTools.rayTrace(pos, start, end, getSelectedBoundingBox().offset(-pos.getX(), -pos.getY(), -pos.getZ())),
+            MiscTools.rayTrace(pos, start, end, world.getBlockState(pos).getBoundingBox(world, pos))
+        );
+
+        RayTraceResult result = null;
+        double min = 0;
+        for (RayTraceResult each : list) {
+            if (each != null) {
+                double d0 = each.hitVec.squareDistanceTo(end);
+                if (d0 > min) {
+                    result = each;
+                    min = d0;
+                }
+            }
+        }
+
+        return result;
     }
 
     @Override
+    @Nullable
     public AxisAlignedBB getCollisionBoundingBox(IBlockState state) {
         return COLLISION_BOX;
     }
