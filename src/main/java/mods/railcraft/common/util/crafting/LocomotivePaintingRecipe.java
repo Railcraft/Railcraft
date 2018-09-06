ec2e83d9aec8b1ad81bde68fc0703ae29d78dc13
liach <liach@users.noreply.github.com>
2018-09-05 17:32:47 -0700

    Fixes locomotive dyeing recipe
    Fixes #1473
    
    Signed-off-by: liach <liach@users.noreply.github.com>

--- src/main/java/mods/railcraft/common/util/crafting/LocomotivePaintingRecipe.java
+++ src/main/java/mods/railcraft/common/util/crafting/LocomotivePaintingRecipe.java
@@ -13,22 +13,23 @@ import mods.railcraft.common.carts.ItemLocomotive;
 import mods.railcraft.common.plugins.color.EnumColor;
 import mods.railcraft.common.plugins.forge.CraftingPlugin;
 import mods.railcraft.common.util.inventory.InvTools;
+import mods.railcraft.common.util.misc.Game;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
-
+import net.minecraftforge.common.crafting.IShapedRecipe;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * @author CovertJaguar <http://www.railcraft.info>
  */
-public class LocomotivePaintingRecipe extends BaseRecipe {
+public class LocomotivePaintingRecipe extends BaseRecipe implements IShapedRecipe {
 
     private final ItemStack locomotive;
 
     public LocomotivePaintingRecipe(ItemStack locomotive) {
-        super("locomotive_painting");
+        super(locomotive.getItem().getRegistryName().getResourcePath() + "_painting");
         this.locomotive = locomotive;
         InvTools.addNBTTag(locomotive, "gregfix", "get the hell off my lawn!");
     }
@@ -54,33 +55,39 @@ public class LocomotivePaintingRecipe extends BaseRecipe {
 
     @Override
     public boolean matches(InventoryCrafting craftingGrid, World var2) {
-        ItemStack dyePrimary = craftingGrid.getStackInRowAndColumn(1, 0);
-        if (!isDye(dyePrimary))
-            return false;
-        ItemStack cart = craftingGrid.getStackInRowAndColumn(1, 1);
-        if (!isLocomotive(cart))
-            return false;
-        ItemStack dyeSecondary = craftingGrid.getStackInRowAndColumn(1, 2);
-        return isDye(dyeSecondary);
+        for (int i = 0; i < 3; i++) {
+            ItemStack dyePrimary = craftingGrid.getStackInRowAndColumn(i, 0);
+            if (!isDye(dyePrimary))
+                continue;
+            ItemStack cart = craftingGrid.getStackInRowAndColumn(i, 1);
+            if (!isLocomotive(cart))
+                continue;
+            ItemStack dyeSecondary = craftingGrid.getStackInRowAndColumn(i, 2);
+            if (isDye(dyeSecondary))
+                return true;
+        }
+        return false;
     }
 
     @Override
     public ItemStack getCraftingResult(InventoryCrafting craftingGrid) {
+        for (int i = 0; i < 3; i++) {
+            ItemStack dyePrimary = craftingGrid.getStackInRowAndColumn(i, 0);
+            ItemStack loco = craftingGrid.getStackInRowAndColumn(i, 1);
+            ItemStack dyeSecondary = craftingGrid.getStackInRowAndColumn(i, 2);
 
-        ItemStack dyePrimary = craftingGrid.getStackInRowAndColumn(1, 0);
-        ItemStack loco = craftingGrid.getStackInRowAndColumn(1, 1);
-        ItemStack dyeSecondary = craftingGrid.getStackInRowAndColumn(1, 2);
+            if (InvTools.isEmpty(loco))
+                continue;
 
-        if (InvTools.isEmpty(loco))
-            return InvTools.emptyStack();
+            EnumColor colorPrimary = getDye(dyePrimary);
+            EnumColor colorSecondary = getDye(dyeSecondary);
 
-        EnumColor colorPrimary = getDye(dyePrimary);
-        EnumColor colorSecondary = getDye(dyeSecondary);
-
-        ItemStack result = loco.copy();
-        if (colorPrimary != null && colorSecondary != null)
-            ItemLocomotive.setItemColorData(result, colorPrimary, colorSecondary);
-        return result;
+            ItemStack result = loco.copy();
+            if (colorPrimary != null && colorSecondary != null)
+                ItemLocomotive.setItemColorData(result, colorPrimary, colorSecondary);
+            return result;
+        }
+        return ItemStack.EMPTY;
     }
 
     @Override
@@ -91,6 +98,16 @@ public class LocomotivePaintingRecipe extends BaseRecipe {
     @Override
     public boolean canFit(int width, int height) {
         return width >= 1 && height >= 3;
+    }
+
+    @Override
+    public int getRecipeWidth() {
+        return 1;
+    }
+
+    @Override
+    public int getRecipeHeight() {
+        return 3;
     }
 
     @Override
