016eb1a309853492f4caabdfeffbdb963e6ed9ad
CovertJaguar <covertjaguar@gmail.com>
2018-11-07 19:07:52 -0800

    Update Forge to latest

--- src/main/java/mods/railcraft/common/core/Railcraft.java
+++ src/main/java/mods/railcraft/common/core/Railcraft.java
@@ -44,7 +44,7 @@ import java.io.File;
         acceptedMinecraftVersions = Railcraft.MC_VERSION,
         guiFactory = "mods.railcraft.client.core.RailcraftGuiConfigFactory",
         updateJSON = "http://www.railcraft.info/railcraft_versions",
-        dependencies = "required-after:forge@[14.23.4.2761,);"
+        dependencies = "required-after:forge@[14.23.5.2772,);"
                 + "after:buildcraftcore@[7.99.17,);"
                 + "after:buildcraftenergy;"
                 + "after:buildcraftbuilders;"
