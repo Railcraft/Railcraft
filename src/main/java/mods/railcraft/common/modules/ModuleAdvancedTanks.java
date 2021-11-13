64fe1748f764601fb6f7e2f55ded31b11330a750
Alkalus <3060479+draknyte1@users.noreply.github.com>
2021-11-13 20:12:11 +0000

    Updated en_US.lang. Fixed a typo.

--- src/main/java/mods/railcraft/common/modules/ModuleAdvancedTanks.java
+++ src/main/java/mods/railcraft/common/modules/ModuleAdvancedTanks.java
@@ -17,7 +17,7 @@ public class ModuleAdvancedTanks extends RailcraftModule {
 	public static GenericMultiTankBase STAINLESS;
 	public static GenericMultiTankBase TITANIUM;
 	public static GenericMultiTankBase TUNGSTENSTEEL;
-	public static GenericMultiTankBase PAlLADIUM;
+	public static GenericMultiTankBase PALLADIUM;
 	public static GenericMultiTankBase IRIDIUM;
 	public static GenericMultiTankBase OSMIUM;
 	public static GenericMultiTankBase NEUTRONIUM;
@@ -26,7 +26,7 @@ public class ModuleAdvancedTanks extends RailcraftModule {
     public final static int CAPACITY_PER_BLOCK_STAINLESS = 96 * FluidHelper.BUCKET_VOLUME;
     public final static int CAPACITY_PER_BLOCK_TITANIUM = 128 * FluidHelper.BUCKET_VOLUME;
     public final static int CAPACITY_PER_BLOCK_TUNGSTENSTEEL = 160 * FluidHelper.BUCKET_VOLUME;
-    public final static int CAPACITY_PER_BLOCK_PAlLADIUM = 192 * FluidHelper.BUCKET_VOLUME;
+    public final static int CAPACITY_PER_BLOCK_PALLADIUM = 192 * FluidHelper.BUCKET_VOLUME;
     public final static int CAPACITY_PER_BLOCK_IRIDIUM = 256 * FluidHelper.BUCKET_VOLUME;
     public final static int CAPACITY_PER_BLOCK_OSMIUM = 384 * FluidHelper.BUCKET_VOLUME;
     public final static int CAPACITY_PER_BLOCK_NEUTRONIUM = 512 * FluidHelper.BUCKET_VOLUME;
@@ -36,7 +36,7 @@ public class ModuleAdvancedTanks extends RailcraftModule {
 		STAINLESS = createTank("stainless", CAPACITY_PER_BLOCK_STAINLESS, EnumMachineZeta.TANK_STAINLESS_WALL, EnumMachineZeta.TANK_STAINLESS_GAUGE, EnumMachineZeta.TANK_STAINLESS_VALVE);
 		TITANIUM = createTank("titanium", CAPACITY_PER_BLOCK_TITANIUM, EnumMachineZeta.TANK_TITANIUM_WALL, EnumMachineZeta.TANK_TITANIUM_GAUGE, EnumMachineZeta.TANK_TITANIUM_VALVE);
 		TUNGSTENSTEEL = createTank("tungstensteel", CAPACITY_PER_BLOCK_TUNGSTENSTEEL, EnumMachineZeta.TANK_TUNGSTENSTEEL_WALL, EnumMachineZeta.TANK_TUNGSTENSTEEL_GAUGE, EnumMachineZeta.TANK_TUNGSTENSTEEL_VALVE);
-		PAlLADIUM = createTank("palladium", CAPACITY_PER_BLOCK_PAlLADIUM, EnumMachineZeta.TANK_PALLADIUM_WALL, EnumMachineZeta.TANK_PALLADIUM_GAUGE, EnumMachineZeta.TANK_PALLADIUM_VALVE);
+		PALLADIUM = createTank("palladium", CAPACITY_PER_BLOCK_PAlLADIUM, EnumMachineZeta.TANK_PALLADIUM_WALL, EnumMachineZeta.TANK_PALLADIUM_GAUGE, EnumMachineZeta.TANK_PALLADIUM_VALVE);
 		IRIDIUM = createTank("iridium", CAPACITY_PER_BLOCK_IRIDIUM, EnumMachineEta.TANK_IRIDIUM_WALL, EnumMachineEta.TANK_IRIDIUM_GAUGE, EnumMachineEta.TANK_IRIDIUM_VALVE);
 		OSMIUM = createTank("osmium", CAPACITY_PER_BLOCK_OSMIUM, EnumMachineEta.TANK_OSMIUM_WALL, EnumMachineEta.TANK_OSMIUM_GAUGE, EnumMachineEta.TANK_OSMIUM_VALVE);
 		NEUTRONIUM = createTank("neutronium", CAPACITY_PER_BLOCK_NEUTRONIUM, EnumMachineEta.TANK_NEUTRONIUM_WALL, EnumMachineEta.TANK_NEUTRONIUM_GAUGE, EnumMachineEta.TANK_NEUTRONIUM_VALVE);
@@ -49,7 +49,7 @@ public class ModuleAdvancedTanks extends RailcraftModule {
 		initTankOfType(STAINLESS);
 		initTankOfType(TITANIUM);
 		initTankOfType(TUNGSTENSTEEL);		
-		initTankOfType(PAlLADIUM);
+		initTankOfType(PALLADIUM);
 		initTankOfType(IRIDIUM);
 		initTankOfType(OSMIUM);
 		initTankOfType(NEUTRONIUM);
