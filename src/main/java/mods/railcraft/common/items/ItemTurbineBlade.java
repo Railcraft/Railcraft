aef9c0475f714cafab2948eeb431f2c201152e09
CovertJaguar <covertjaguar@gmail.com>
2022-05-31 17:44:04 -0700

    Closes #2028 Add steel turbine blade recipe and move both recipes to the Rolling Machine.

--- src/main/java/mods/railcraft/common/items/ItemTurbineBlade.java
+++ src/main/java/mods/railcraft/common/items/ItemTurbineBlade.java
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------
- Copyright (c) CovertJaguar, 2011-2019
+ Copyright (c) CovertJaguar, 2011-2022
  http://railcraft.info
 
  This code is the property of CovertJaguar
@@ -10,21 +10,27 @@
 
 package mods.railcraft.common.items;
 
-import mods.railcraft.common.plugins.forge.CraftingPlugin;
+import mods.railcraft.api.crafting.Crafters;
 import net.minecraft.item.ItemStack;
 
 /**
  * Created by CovertJaguar on 5/19/2016 for Railcraft.
- *e
+ * e
+ *
  * @author CovertJaguar <http://www.railcraft.info>
  */
 public class ItemTurbineBlade extends ItemRailcraft {
     @Override
     public void defineRecipes() {
-        CraftingPlugin.addShapedRecipe(new ItemStack(this),
-                "I",
-                "I",
-                "I",
+        Crafters.rollingMachine().newRecipe(new ItemStack(this)).shaped(
+                "  I",
+                " I ",
+                "I  ",
                 'I', "plateNickel");
+        Crafters.rollingMachine().newRecipe(new ItemStack(this)).shaped(
+                "  I",
+                " I ",
+                "I  ",
+                'I', "plateSteel");
     }
 }
