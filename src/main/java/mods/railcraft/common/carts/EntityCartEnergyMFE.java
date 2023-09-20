86655b5c03104d123af8a5729b0fd4e944cc07d6
AlphaShippai <82609427+AlphaShippai@users.noreply.github.com>
2023-09-19 23:59:35 -0400

    Update EntityCartEnergyMFE.java

--- src/main/java/mods/railcraft/common/carts/EntityCartEnergyMFE.java
+++ src/main/java/mods/railcraft/common/carts/EntityCartEnergyMFE.java
@@ -17,7 +17,7 @@ import net.minecraft.world.World;
  * @author CovertJaguar <http://www.railcraft.info>
  */
 public final class EntityCartEnergyMFE extends EntityCartEnergy {
-    private final int TIER = IC2Plugin.isClassic() ? 2 : 3;
+    private final int TIER = IC2Plugin.isClassic() ? 1 : 1;
     private final int CAPACITY = IC2Plugin.isClassic() ? 600000 : 4000000;
     private final int TRANSFER = IC2Plugin.isClassic() ? 128 : 512;
 
